47b03a89e623abc4d1d558cb4f4c8655de8b9dcc
edisonxiang <xiang.edison@gmail.com>
2019-09-30 13:33:54 +0800

    finish init handler

--- pkg/cibot/inithandler.go
+++ pkg/cibot/inithandler.go
@@ -9,6 +9,10 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+var (
+	DefaultOrg = "openeuler"
+)
+
 type InitHandler struct {
 	Config       Config
 	Context      context.Context
@@ -21,10 +25,10 @@ type ProjectsFile struct {
 }
 
 type Project struct {
-	Name        string `yaml:"name"`
-	Owner       string `yaml:"owner"`
-	Type        string `yaml:"type"`
-	Description string `yaml:"description"`
+	Name        *string `yaml:"name"`
+	Owner       *string `yaml:"owner"`
+	Type        *string `yaml:"type"`
+	Description *string `yaml:"description"`
 }
 
 // Serve
@@ -43,4 +47,48 @@ func (handler *InitHandler) Serve() {
 	}
 
 	glog.Infof("projects: %v", projects)
+	for i := 0; i < len(projects.Projects); i++ {
+		// log
+		p := projects.Projects[i]
+		glog.Infof("begin to run: %s", *p.Name)
+
+		// build create repository param
+		repobody := gitee.RepositoryPostParam{}
+		repobody.AccessToken = handler.Config.GiteeToken
+		repobody.Name = *p.Name
+		repobody.Description = *p.Description
+		repobody.HasIssues = true
+		repobody.HasWiki = true
+		if *p.Type == "private" {
+			repobody.Public = 0
+		} else {
+			repobody.Public = 1
+		}
+
+		// invoke create repository
+		glog.Infof("begin to create repository: %s", *p.Name)
+		_, _, err = handler.GiteeClient.RepositoriesApi.PostV5OrgsOrgRepos(handler.Context, DefaultOrg, repobody)
+		if err != nil {
+			glog.Errorf("fail to create repository: %v", err)
+			continue
+		}
+		glog.Infof("end to create repository: %s", *p.Name)
+
+		// build create project member param
+		memberbody := gitee.ProjectMemberPutParam{}
+		memberbody.AccessToken = handler.Config.GiteeToken
+		memberbody.Permission = "admin"
+
+		// invoke create project member
+		glog.Infof("begin to create project member: %s", *p.Name)
+		_, _, err = handler.GiteeClient.RepositoriesApi.PutV5ReposOwnerRepoCollaboratorsUsername(handler.Context, DefaultOrg, *p.Name, *p.Owner, memberbody)
+		if err != nil {
+			glog.Errorf("fail to create project member: %v", err)
+			continue
+		}
+		glog.Infof("end to create project member: %s", *p.Name)
+
+		// log
+		glog.Infof("end to run: %s", *p.Name)
+	}
 }
