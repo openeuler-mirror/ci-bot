00adbc971996cec867610a1e4315e1f3eb95e6ad
xiangxinyong <xiangxingyong@qq.com>
2019-10-21 16:23:25 +0800

    close and reopen in issue

--- pkg/cibot/reopen.go
+++ pkg/cibot/reopen.go
@@ -55,6 +55,52 @@ func (s *Server) ReOpen(event *gitee.NoteEvent) error {
 				}
 			}
 		}
+	} else if *event.NoteableType == "Issue" {
+		// handle open
+		if event.Issue.State == "closed" {
+			// get basic informations
+			comment := event.Comment.Body
+			owner := event.Repository.Namespace
+			repo := event.Repository.Name
+			issueNumber := event.Issue.Number
+			issueAuthor := event.Issue.User.Login
+			commentAuthor := event.Comment.User.Login
+			glog.Infof("reopen started. comment: %s owner: %s repo: %s issueNumber: %s issueAuthor: %s commentAuthor: %s",
+				comment, owner, repo, issueNumber, issueAuthor, commentAuthor)
+
+			// check if current author has write permission
+			localVarOptionals := &gitee.GetV5ReposOwnerRepoCollaboratorsUsernamePermissionOpts{}
+			localVarOptionals.AccessToken = optional.NewString(s.Config.GiteeToken)
+			// get permission
+			permission, _, err := s.GiteeClient.RepositoriesApi.GetV5ReposOwnerRepoCollaboratorsUsernamePermission(
+				s.Context, owner, repo, commentAuthor, localVarOptionals)
+			if err != nil {
+				glog.Errorf("unable to get comment author permission: %v", err)
+				return err
+			}
+			// permission: admin, write, read, none
+			if permission.Permission == "admin" || permission.Permission == "write" || issueAuthor == commentAuthor {
+				//  issue author or permission: admin, write
+				body := gitee.IssueUpdateParam{}
+				body.Repo = repo
+				body.AccessToken = s.Config.GiteeToken
+				body.State = "open"
+				glog.Infof("invoke api to reopen: %s", issueNumber)
+
+				// patch state
+				_, response, err := s.GiteeClient.IssuesApi.PatchV5ReposOwnerIssuesNumber(s.Context, owner, issueAuthor, body)
+				if err != nil {
+					if response.StatusCode == 400 {
+						glog.Infof("reopen successfully with status code %d: %s", response.StatusCode, issueNumber)
+					} else {
+						glog.Errorf("unable to reopen: %s err: %v", issueNumber, err)
+						return err
+					}
+				} else {
+					glog.Infof("reopen successfully: %v", issueNumber)
+				}
+			}
+		}
 	}
 	return nil
 }
