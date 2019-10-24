72e4b3fc75a07ed01b5a38188a16b0eaf186bd4a
xiangxinyong <xiangxingyong@qq.com>
2019-10-24 17:53:58 +0800

    remove assignees and test before merge

--- pkg/cibot/pullrequest.go
+++ pkg/cibot/pullrequest.go
@@ -1,7 +1,10 @@
 package cibot
 
 import (
+	"strings"
+
 	"gitee.com/openeuler/go-gitee/gitee"
+	"github.com/antihax/optional"
 	"github.com/golang/glog"
 )
 
@@ -22,6 +25,74 @@ func (s *Server) HandlePullRequestEvent(event *gitee.PullRequestEvent) {
 			glog.Errorf("failed to check cla by pull request event: %v", err)
 		}
 	}
+}
+
+// RemoveAssigneesInPullRequest remove assignees in pull request
+func (s *Server) RemoveAssigneesInPullRequest(event *gitee.NoteEvent) error {
+	if event != nil {
+		if event.PullRequest != nil {
+			assignees := event.PullRequest.Assignees
+			glog.Infof("remove assignees: %v", assignees)
+			if len(assignees) > 0 {
+				var strAssignees string
+				for _, assignee := range assignees {
+					strAssignees += assignee.Login + ","
+				}
+				strAssignees = strings.TrimRight(strAssignees, ",")
+				glog.Infof("remove assignees str: %s", strAssignees)
+
+				// get basic params
+				owner := event.Repository.Namespace
+				repo := event.Repository.Name
+				prNumber := event.PullRequest.Number
+				localVarOptionals := &gitee.DeleteV5ReposOwnerRepoPullsNumberAssigneesOpts{}
+				localVarOptionals.AccessToken = optional.NewString(s.Config.GiteeToken)
+
+				// invoke api
+				_, _, err := s.GiteeClient.PullRequestsApi.DeleteV5ReposOwnerRepoPullsNumberAssignees(s.Context, owner, repo, prNumber, strAssignees, localVarOptionals)
+				if err != nil {
+					glog.Errorf("unable to remove assignees in pull request. err: %v", err)
+					return err
+				}
+				glog.Infof("remove assignees successfully: %s", strAssignees)
+			}
+		}
+	}
+	return nil
+}
+
+// RemoveTestersInPullRequest remove testers in pull request
+func (s *Server) RemoveTestersInPullRequest(event *gitee.NoteEvent) error {
+	if event != nil {
+		if event.PullRequest != nil {
+			testers := event.PullRequest.Testers
+			glog.Infof("remove testers: %v", testers)
+			if len(testers) > 0 {
+				var strTesters string
+				for _, tester := range testers {
+					strTesters += tester.Login + ","
+				}
+				strTesters = strings.TrimRight(strTesters, ",")
+				glog.Infof("remove testers str: %s", strTesters)
+
+				// get basic params
+				owner := event.Repository.Namespace
+				repo := event.Repository.Name
+				prNumber := event.PullRequest.Number
+				localVarOptionals := &gitee.DeleteV5ReposOwnerRepoPullsNumberTestersOpts{}
+				localVarOptionals.AccessToken = optional.NewString(s.Config.GiteeToken)
+
+				// invoke api
+				_, _, err := s.GiteeClient.PullRequestsApi.DeleteV5ReposOwnerRepoPullsNumberTesters(s.Context, owner, repo, prNumber, strTesters, localVarOptionals)
+				if err != nil {
+					glog.Errorf("unable to remove testers in pull request. err: %v", err)
+					return err
+				}
+				glog.Infof("remove testers successfully: %s", strTesters)
+			}
+		}
+	}
+	return nil
 }
 
 // MergePullRequest with lgtm and approved label
@@ -57,6 +128,18 @@ func (s *Server) MergePullRequest(event *gitee.NoteEvent) error {
 	if hasApproved && hasLgtm {
 		// current pr can be merged
 		if event.PullRequest.Mergeable {
+			// remove assignees
+			err = s.RemoveAssigneesInPullRequest(event)
+			if err != nil {
+				glog.Errorf("unable to remove assignees. err: %v", err)
+				return err
+			}
+			// remove testers
+			err = s.RemoveTestersInPullRequest(event)
+			if err != nil {
+				glog.Errorf("unable to remove testers. err: %v", err)
+				return err
+			}
 			// merge pr
 			body := gitee.PullRequestMergePutParam{}
 			body.AccessToken = s.Config.GiteeToken
